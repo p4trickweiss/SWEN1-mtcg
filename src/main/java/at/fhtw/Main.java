0ca0acd3d2cbf0e170320ea8f51d00e7dabd349a
frabfrab <patrick1610@gmx.at>
2022-11-04 14:34:43 +0100

    implemented Http Server with Tests

---
+++ src/main/java/at/fhtw/Main.java
@@ -0,0 +1,25 @@
+package at.fhtw;
+
+import at.fhtw.httpserver.server.Server;
+import at.fhtw.httpserver.utils.Router;
+import at.fhtw.mtcgapp.service.echo.EchoService;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        Server server = new Server(10001, configureRouter());
+        try {
+            server.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Router configureRouter() {
+        Router router = new Router();
+        router.addService("/echo", new EchoService());
+
+        return router;
+    }
+}
