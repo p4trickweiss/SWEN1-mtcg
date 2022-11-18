b3bb7efc1e95f64cb1b162fa982ce199c01a4b24
Patrick Weiss <p4trick.weiss@gmail.com>
2022-11-18 15:26:03 +0100

    improved sessionsController

--- src/main/java/at/fhtw/mtcgapp/service/session/SessionsController.java
+++ src/main/java/at/fhtw/mtcgapp/service/session/SessionsController.java
@@ -5,34 +5,55 @@ import at.fhtw.httpserver.http.HttpStatus;
 import at.fhtw.httpserver.server.Request;
 import at.fhtw.httpserver.server.Response;
 import at.fhtw.mtcgapp.controller.Controller;
+import at.fhtw.mtcgapp.dal.UOW;
+import at.fhtw.mtcgapp.dal.repos.UserRepo;
 import at.fhtw.mtcgapp.model.User;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
+import java.sql.SQLException;
+
 public class SessionsController extends Controller {
 
-    private final SessionsUOW sessionsUOW;
+    private final UOW uow;
 
-    public SessionsController() { sessionsUOW = new SessionsUOW(); }
+    public SessionsController() { uow = new UOW(); }
 
     public Response logIn(Request request) {
         try {
             User user = this.getObjectMapper().readValue(request.getBody(), User.class);
             try {
-                this.sessionsUOW.logIn(user);
-            } catch (Exception e) {
-                if(e.getMessage() == "Invalid username/password provided")
+                UserRepo userRepo = new UserRepo(uow.getConnection());
+                uow.getConnection().setAutoCommit(false);
+                User userdata = userRepo.getUser(user.getUsername());
+                if(user.getUsername().equals(userdata.getUsername()) && user.getPassword().equals(userdata.getPassword())){
+                    user.setToken(user.getUsername() + "-mtcgToken");
+                    userRepo.createSession(user);
+                } else {
+                    uow.getConnection().commit();
                     return new Response(
                             HttpStatus.UNAUTHORIZED,
                             ContentType.JSON,
-                            "{ message : \"Invalid username/password provided\" }"
+                            "{ \"message\" : \"Invalid username/password\" }"
                     );
+                }
+               uow.getConnection().commit();
+                return new Response(
+                        HttpStatus.OK,
+                        ContentType.JSON,
+                        user.getToken()
+                );
+            } catch (SQLException sqlException)  {
+                sqlException.printStackTrace();
+                if (uow.getConnection() != null) {
+                    try {
+                        System.err.print("Transaction is being rolled back");
+                        uow.getConnection().rollback();
+                    } catch (SQLException excep) {
+                        excep.printStackTrace();
+                    }
+                    }
             }
 
-            return new Response(
-                    HttpStatus.OK,
-                    ContentType.JSON,
-                    user.getToken()
-            );
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
