3a4f45595e2ecf756615e15d813e1e339d62285f
Patrick Weiss <p4trick.weiss@gmail.com>
2022-12-13 17:16:42 +0100

    updated UOW

--- src/main/java/at/fhtw/mtcgapp/dal/UOW.java
+++ src/main/java/at/fhtw/mtcgapp/dal/UOW.java
@@ -1,6 +1,8 @@
 package at.fhtw.mtcgapp.dal;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class UOW {
 
@@ -8,14 +10,61 @@ public class UOW {
 
     public UOW() {
         this.connection = DbSingleton.INSTANCE.getConnection();
+        try {
+            this.connection.setAutoCommit(false);
+        }
+        catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
     }
 
     public Connection getConnection() {
         return connection;
     }
 
-    public void commit() {
-        
+    public void commitTransaction()
+    {
+        if (this.connection != null) {
+            try {
+                this.connection.commit();
+            } catch (SQLException e) {
+                throw new DataAccessException("Commit der Transaktion nicht erfolgreich", e);
+            }
+        }
+    }
+    public void rollbackTransaction()
+    {
+        if (this.connection != null) {
+            try {
+                this.connection.rollback();
+            } catch (SQLException e) {
+                throw new DataAccessException("Rollback der Transaktion nicht erfolgreich", e);
+            }
+        }
+    }
+
+    public void finishWork()
+    {
+        if (this.connection != null) {
+            try {
+                this.connection.close();
+                //this.connection = null;
+            } catch (SQLException e) {
+                throw new DataAccessException("Schließen der Connection nicht erfolgreich", e);
+            }
+        }
+    }
+
+    public PreparedStatement prepareStatement(String sql)
+    {
+        if (this.connection != null) {
+            try {
+                return this.connection.prepareStatement(sql);
+            } catch (SQLException e) {
+                throw new DataAccessException("Erstellen eines PreparedStatements nicht erfolgreich", e);
+            }
+        }
+        throw new DataAccessException("UnitOfWork hat keine aktive Connection zur Verfügung");
     }
 }
 
